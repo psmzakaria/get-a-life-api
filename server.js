d392f12e390786052cd2e393ebc0163be49ca96c
Derek <derek.chan522@gmail.com>
2018-07-30 14:49:04 +0800

    add mongoose connect

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const app = require("./app");
+const mongodbUri = process.env.MONGODB_URI || "mongodb://localhost/jumpstart";
+
+mongoose.connect(mongodbUri);
+const db = mongoose.connection;
+db.on("error", error => {
+  console.error("An error occured!", error);
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`get-a-life-api has started on PORT ${server.address().port}`);
