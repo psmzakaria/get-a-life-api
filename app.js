a3c86327d3e6bfc0dc477cd33536567d04b40b7a
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-07-30 17:31:27 +0800

    Added users route in app

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 const app = express();
 
-const indexRouter = require("./routers/index");
+const indexRouter = require('./routers/index');
+const usersRouter = require('./routers/usersRoute');
 
-if (process.env.NODE_ENV === "production") {
-    app.use(cors({ origin: process.env.GAL_UI_URL }));
+if (process.env.NODE_ENV === 'production') {
+	app.use(cors({ origin: process.env.GAL_UI_URL }));
 } else {
-    app.use(cors());
+	app.use(cors());
 }
 
 app.use(express.json());
 
 indexRouter(app);
+usersRouter(app);
 
 module.exports = app;
