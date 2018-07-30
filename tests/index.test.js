9fd193400657e1701bc740d294803a821eea37e6
Derek <derek.chan522@gmail.com>
2018-07-30 14:50:27 +0800

    add dummy supertest

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,3 +1,14 @@
-test("should ", () => {
-  expect(1).toBe(1);
+const express = require("express");
+const request = require("supertest");
+
+const indexRouter = require("../routers/index");
+
+const app = express();
+
+indexRouter(app);
+
+test("GET /", async () => {
+  const response = await request(app).get("/");
+
+  expect(response.status).toBe(200);
 });
