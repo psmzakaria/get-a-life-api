5aec5107f8464f667682b143740db4a405800d20
Derek <derek.chan522@gmail.com>
2018-07-31 12:17:09 +0800

    refactor usersRoute.js and usersRoute.test.js

--- tests/usersRoute.test.js
+++ tests/usersRoute.test.js
@@ -19,7 +19,6 @@ const userNoUsername = {
   password: "password"
 };
 
-// before all test blocks
 beforeAll(async () => {
   jest.setTimeout(120000);
 
@@ -32,51 +31,46 @@ afterAll(() => {
   mongod.stop();
 });
 
-test("GET /users should return response 200", async () => {
-  const response = await request(app).get("/users");
-  expect(response.status).toBe(200);
+describe("POST /signup", () => {
+  test('post a unique new user should return response status 201"', async () => {
+    const response = await request(app)
+      .post("/users/signup")
+      .send(user01);
+
+    expect(response.status).toBe(201);
+  });
+
+  test("post a duplicate new user should return response status 500", async () => {
+    const response = await request(app)
+      .post("/users/signup")
+      .send(user01);
+    expect(response.status).toBe(500);
+  });
+
+  test("post a new user with no username should return response status 500", async () => {
+    const response = await request(app)
+      .post("/users/signup")
+      .send(userNoUsername);
+    expect(response.status).toBe(500);
+  });
 });
 
-test('POST /signup post a unique new user should return response status 201"', async () => {
-  const response = await request(app)
-    .post("/users/signup")
-    .send(user01);
+describe("POST /signin", () => {
+  test("should return a status of 201 with valid username and password", async () => {
+    const response = await request(app)
+      .post("/users/signin")
+      .send(user01);
+    expect(response.status).toBe(201);
+  });
 
-  expect(response.status).toBe(201);
-});
-
-// invalid signup - duplicate user
-test("POST /signup - post a duplicate new user should return response status 500", async () => {
-  const response = await request(app)
-    .post("/users/signup")
-    .send(user01);
-  expect(response.status).toBe(500);
-});
-
-// invalid signup - no username
-test("POST /signup - post a new user with no username should return response status 500", async () => {
-  const response = await request(app)
-    .post("/users/signup")
-    .send(userNoUsername);
-  expect(response.status).toBe(500);
-});
-
-// sign a user
-test("POST /signin should return a status of 201", async () => {
-  const response = await request(app)
-    .post("/users/signin")
-    .send(user01);
-  expect(response.status).toBe(201);
-});
-
-// invalid signin - invalid password
-test("POST /signin - post a existing user with an existing username with false password should return response status 500", async () => {
-  const response = await request(app)
-    .post("/users/signin")
-    .send({
-      username: user01.username,
-      password: "wrong password"
-    });
-  expect(response.status).toBe(401);
-  expect(response.body.message).toBe("passwords did not match");
+  test("should return a status of 401 with an existing username but false password", async () => {
+    const response = await request(app)
+      .post("/users/signin")
+      .send({
+        username: user01.username,
+        password: "wrong password"
+      });
+    expect(response.status).toBe(401);
+    expect(response.body.message).toBe("passwords did not match");
+  });
 });
