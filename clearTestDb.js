ca907c85905075f8a43adafd3059ad602750d3c1
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-08-02 15:04:01 +0800

    Initialised e2e testing script to create a new test database

---
+++ clearTestDb.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const mongodbUri = process.env.MONGODB_URI || 'mongodb://localhost/jumpstart-test';
+
+mongoose.connect(mongodbUri, async () => {
+	await mongoose.connection.db.dropDatabase();
+	mongoose.connection.close();
+});
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const app = require("./app");
-const mongoose = require("mongoose");
+const app = require('./app');
+const mongoose = require('mongoose');
 
 const mongodbUri = process.env.MONGODB_URI || "mongodb://localhost:27017/jumpstart";
 
@@ -8,10 +8,10 @@ mongoose.connect(
   { useNewUrlParser: true }
 );
 const db = mongoose.connection;
-db.on("error", error => {
-  console.error("An error occured!", error);
+db.on('error', (error) => {
+	console.error('An error occured!', error);
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log(`get-a-life-api has started on PORT ${server.address().port}`);
+	console.log(`get-a-life-api has started on PORT ${server.address().port}`);
 });
