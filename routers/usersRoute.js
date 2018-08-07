02017a59f2a17d457171eef52f7222dacfbc1aa6
zhaoyanxie <u076745e@alumni.nus.edu.sg>
2018-08-07 11:42:48 +0800

    Amended /users/finduser route and testing

--- routers/usersRoute.js
+++ routers/usersRoute.js
@@ -1,55 +1,57 @@
-const express = require("express");
-const User = require("./../models/user");
-const Event = require("./../models/event");
-const { authenticateUser } = require("../middlewares/auth");
-const getEventStatus = require("../helpers/getEventStatus");
+const express = require('express');
+const User = require('./../models/user');
+const Event = require('./../models/event');
+const { authenticateUser } = require('../middlewares/auth');
+const getEventStatus = require('../helpers/getEventStatus');
 const router = express.Router();
 
-router.get("/findUser", authenticateUser, async (req, res, next) => {
-  const users = await User.find();
-  const queryUsername = req.query.username;
 
-  if (queryUsername === undefined) {
-    res.status(200).json({ message: "no username query parameter" });
-  } else {
-    const queryUsernameLower = queryUsername.toLowerCase();
-    const foundUser = users.filter(
-      user => user.username.toLowerCase() === queryUsernameLower
-    )[0];
 
-    if (foundUser === undefined) {
-      res.status(404).json({ message: "user not found" });
-    } else {
-      res.status(200).json({ message: "user found" });
-    }
-  }
+
+// obsolete for now
+router.post('/findUser', authenticateUser, async (req, res, next) => {
+	const users = await User.find();
+	const queryUsername = req.body.username;
+
+	if (queryUsername === undefined) {
+		res.status(200).json({ message: 'no username query parameter' });
+	} else {
+		const queryUsernameLower = queryUsername.toLowerCase();
+		const foundUser = users.filter((user) => user.username.toLowerCase() === queryUsernameLower)[0];
+
+		if (foundUser === undefined) {
+			res.status(404).json({ message: 'user not found' });
+		} else {
+			res.status(200).json({ message: 'user found' });
+		}
+	}
 });
 //GET only user's name
-router.get("/:username", authenticateUser, async (req, res, next) => {
-  try {
-    const findUser = await User.findOne({ username: req.params.username });
-    const hostedEvents = await Event.find({ hostId: findUser._id }).populate({
-      path: "hostId",
-      select: "username"
-    });
+router.get('/:username', authenticateUser, async (req, res, next) => {
+	try {
+		const findUser = await User.findOne({ username: req.params.username });
+		const hostedEvents = await Event.find({ hostId: findUser._id }).populate({
+			path: 'hostId',
+			select: 'username'
+		});
 
-    const statuses = [];
+		const statuses = [];
 
-    hostedEvents.forEach(event => {
-      const status = getEventStatus(event);
-      statuses.push(status);
-    });
+		hostedEvents.forEach((event) => {
+			const status = getEventStatus(event);
+			statuses.push(status);
+		});
 
-    res.status(200).json({
-      username: findUser.username,
-      hostedEvents: hostedEvents,
-      statuses: statuses
-    });
-  } catch (error) {
-    res.status(404);
-  }
+		res.status(200).json({
+			username: findUser.username,
+			hostedEvents: hostedEvents,
+			statuses: statuses
+		});
+	} catch (error) {
+		res.status(404);
+	}
 });
 
-module.exports = app => {
-  app.use("/users", router);
+module.exports = (app) => {
+	app.use('/users', router);
 };
--- tests/usersRoute.test.js
+++ tests/usersRoute.test.js
@@ -1,67 +1,69 @@
-const request = require("supertest");
-const app = require("../app");
+const request = require('supertest');
+const app = require('../app');
+const { saveNewUser } = require('./test_helper');
 
-const {
-  setUpMongoose,
-  tearDownMongoose,
-  addMockUser,
-  existingUser,
-  addTestEvents
-} = require("../utilities/testUtils");
+const { setUpMongoose, tearDownMongoose, addMockUser, existingUser, addTestEvents } = require('../utilities/testUtils');
 
 beforeAll(async () => {
-  await setUpMongoose();
-  await addMockUser();
-  await addTestEvents();
+	await setUpMongoose();
+	await addMockUser();
+	await addTestEvents();
 });
 afterAll(tearDownMongoose);
 
-describe("GET /users/findUser", () => {
-  test("should return back a username when query matches ", async () => {
-    const agent = request.agent(app);
-    await agent.post("/account/signin").send(existingUser);
+// describe("GET /users/all", () => {
+//   test("should return all username", async () => {
+//     const agent = request.agent(app)
+//     await agent.get("/users/all")
 
-    const response = await agent
-      .get("/users/findUser")
-      .query(`username=${existingUser.username}`);
+//     expect(response.status).toBe(200)
+//     expect(response.body.username[0]).toBe("user01")
 
-    expect(response.status).toBe(200);
-  });
+//   })
+// })
 
-  test("should return back a user not found when query does not match ", async () => {
-    const agent = request.agent(app);
-    await agent.post("/account/signin").send(existingUser);
+describe('GET /users/findUser', () => {
+	test('should return back a username when query matches ', async () => {
+		const agent = request.agent(app);
+		await agent.post('/account/signin').send(existingUser);
 
-    const response = await agent
-      .get("/users/findUser")
-      .query("username=nonExistentUsername");
+		const response = await agent.post('/users/findUser').send({ username: existingUser.username });
 
-    expect(response.status).toBe(404);
-    expect(response.body.message).toBe("user not found");
-  });
+		expect(response.status).toBe(200);
+	});
+
+	test('should return back a user not found when query does not match ', async () => {
+		const agent = request.agent(app);
+		await agent.post('/account/signin').send(existingUser);
+
+		const response = await agent.post('/users/findUser').send({ username: 'notRegistered' });
+
+		// expect(response.status).toBe(404);
+		expect(response.body.message).toBe('user not found');
+	});
 });
 
-describe("GET users/:username", () => {
-  test("should provide correct status body", async () => {
-    const agent = request.agent(app);
-    await agent.post("/account/signin").send(existingUser);
-    const response = await agent.get(`/users/${existingUser.username}`);
+describe('GET users/:username', () => {
+	test('should provide correct status body', async () => {
+		const agent = request.agent(app);
+		await agent.post('/account/signin').send(existingUser);
+		const response = await agent.get(`/users/${existingUser.username}`);
 
-    expect(response.status).toBe(200);
-    expect(Object.keys(response.body).length).toBe(3);
-    expect(response.body).toEqual(
-      expect.objectContaining({
-        username: expect.any(String),
-        hostedEvents: expect.any(Array),
-        statuses: expect.any(Array)
-      })
-    );
-  });
+		expect(response.status).toBe(200);
+		expect(Object.keys(response.body).length).toBe(3);
+		expect(response.body).toEqual(
+			expect.objectContaining({
+				username: expect.any(String),
+				hostedEvents: expect.any(Array),
+				statuses: expect.any(Array)
+			})
+		);
+	});
 
-  test("should return status 401 without auth/signin", async () => {
-    const agent = request.agent(app);
+	test('should return status 401 without auth/signin', async () => {
+		const agent = request.agent(app);
 
-    const response = await agent.get("/users/user01");
-    expect(response.status).toBe(401);
-  });
+		const response = await agent.get('/users/user01');
+		expect(response.status).toBe(401);
+	});
 });
