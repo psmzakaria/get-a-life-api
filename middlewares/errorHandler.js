5a1b3d1d237838eef145001fac6a016f8fa104db
huichingheng <huichingheng@gmail.com>
2018-08-07 12:26:29 +0800

    Make errorHandler middleware more readable

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,6 @@
 const errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500).json({
+  res.status(err.status || 500)
+  res.json({
     errors: {
       message: err.message
     }
