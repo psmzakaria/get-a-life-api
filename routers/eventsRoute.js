ebee0bd90eef8eb73342571604abd2f2800a8470
huichingheng <huichingheng@gmail.com>
2018-08-07 16:51:26 +0800

    add hostname in response for GET /events/:id

--- routers/eventsRoute.js
+++ routers/eventsRoute.js
@@ -1,50 +1,61 @@
-const express = require('express');
-const { passport } = require('../config/passport');
-const eachDay = require('date-fns/each_day');
-const format = require('date-fns/format');
-const Event = require('./../models/event');
-const getDate = require('../helpers/getDate');
+const express = require("express");
+const { passport } = require("../config/passport");
+const eachDay = require("date-fns/each_day");
+const format = require("date-fns/format");
+const Event = require("./../models/event");
+const getDate = require("../helpers/getDate");
 
 const router = express.Router();
 
-router.post('/create', passport.authenticate('jwt', { session: false }), async (req, res, next) => {
-	try {
-		const startDate = getDate(req.body.startDate);
-		const endDate = getDate(req.body.endDate);
+router.post(
+  "/create",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res, next) => {
+    try {
+      const startDate = getDate(req.body.startDate);
+      const endDate = getDate(req.body.endDate);
 
-		const result = eachDay(startDate, endDate);
+      const result = eachDay(startDate, endDate);
 
-		const newEvent = new Event({
-			title: req.body.title,
-			proposedDates: result.map((date) => {
-				return format(date, 'YYYYMMDD');
-			}),
-			hostId: req.user._id
-		});
-		const event = await newEvent.save();
+      const newEvent = new Event({
+        title: req.body.title,
+        proposedDates: result.map(date => {
+          return format(date, "YYYYMMDD");
+        }),
+        hostId: req.user._id
+      });
+      const event = await newEvent.save();
 
-		res.status(201).json(event);
-	} catch (error) {
-		next(error);
-	}
+      res.status(201).json(event);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.get("/:id", async (req, res, next) => {
+  const eventId = req.params.id;
+  try {
+    const event = await Event.findById(eventId).populate("hostId");
+    if (event === null) {
+      payload = null;
+    } else {
+      payload = {
+        ...event._doc,
+        hostName: event.hostId.username,
+        hostId: event.hostId._id
+      };
+    }
+
+    res.json({ payload });
+  } catch (error) {
+    if (error.name === "CastError") {
+      error.status = 400;
+    }
+    next(error);
+  }
 });
 
-router.get('/:id', async (req, res, next) => {
-	const eventId = req.params.id;
-	try {
-		const event = await Event.findById(eventId);
-
-		res.json({
-			payload: event
-		});
-	} catch (error) {
-		if (error.name === 'CastError') {
-			error.status = 400;
-		}
-		next(error);
-	}
-});
-
-module.exports = (app) => {
-	app.use('/events', router);
+module.exports = app => {
+  app.use("/events", router);
 };
