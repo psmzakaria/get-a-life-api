117ce25861c3ccb459561dc0ed35ef1066ddfdb3
psmzakaria <psmzakaria@gmail.com>
2018-08-16 11:54:34 +0800

    Add exclamation mark to differentiate between production and staging apis

--- routers/index.js
+++ routers/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res, next) => {
-  res.json({ message: "Welcome to Get-A-Life, setup some activity!" });
+  res.json({ message: "Welcome to Get-A-Life, setup some activity!!" });
 });
 
 module.exports = app => {
